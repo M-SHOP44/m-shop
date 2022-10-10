481369c2e167435b782a1412ca3f1dbbff49f5d9
emnabz <1947@holbertonschool.com>
2022-10-10 15:03:23 +0100

    add

--- front-side/src/components/Shopkeeper/Dashboard/Shopowner.jsx
+++ front-side/src/components/Shopkeeper/Dashboard/Shopowner.jsx
@@ -2,55 +2,62 @@ import React from 'react'
 import '../Dashboard/style.css'
 import { FcGrid,FcBookmark,FcSettings,FcMenu } from "react-icons/fc";
 import Add from '../Products/Add'
+import { Link } from "react-router-dom";
+
 
 const Shopowner = () => {
 
 
   return (
-    < div  className='container-shopowner'>
+    <><header className="main-nav">
+      <Link to="/" id="myshop">
+        M-shop
+      </Link>
+    </header>
+    <div className='container-shopowner'>
         <nav className='nav-shop'>
           <div className='select'>
-          {/* <span ><FcGrid font-size="20px"/></span> */}
-          <select name='product' id="select1">
-            <option value="manage">Manage Products</option>
-            <option value="add">Add product</option>
-            <option value="import">Import product</option>
-            <option value="images">manage Images</option>
-          </select>
+            {/* <span ><FcGrid font-size="20px"/></span> */}
+            <select name='product' id="select1">
+              <option value="manage">Manage Products</option>
+              <option value="add">Add product</option>
+              <option value="import">Import product</option>
+              <option value="images">manage Images</option>
+            </select>
           </div>
-         <div className='select'>
-          {/* <span><FcBookmark font-size="20px"/></span> */}
-         <select name='commande' id="select2">
-            <option value="product">Manage commandes</option>
-          </select>
-         </div>
-         <div className='select'>
-         {/* <span><Settings font-size="20px"/></span> */}
-         <select name='settings' id="select3">
-            <option value="profil">your profil</option>
-          </select>
-         </div>
-         <div className='select'>
-         <select name="aide" id="select4">
-         <option value="0">Help</option>
-         <option value="documentation">Documentation api</option>
-          </select>
-         </div>
           <div className='select'>
-          <select name='logout' id="select5" >
-            <option value="notification">preference notification</option>
-            <option value="change">change your password</option>
-            <option value="logout">Logout</option>
-            {/* <span><FcMenu font-size="20px"/></span> */}
-          </select>
-          
+            {/* <span><FcBookmark font-size="20px"/></span> */}
+            <select name='commande' id="select2">
+              <option value="product">Manage commandes</option>
+            </select>
+          </div>
+          <div className='select'>
+            {/* <span><Settings font-size="20px"/></span> */}
+            <select name='settings' id="select3">
+              <option value="profil">your profil</option>
+            </select>
+          </div>
+          <div className='select'>
+            <select name="aide" id="select4">
+              <option value="0">Help</option>
+              <option value="documentation">Documentation api</option>
+            </select>
+          </div>
+          <div className='select'>
+            <select name='logout' id="select5">
+              <option value="notification">preference notification</option>
+              <option value="change">change your password</option>
+              <option value="logout">Logout</option>
+              {/* <span><FcMenu font-size="20px"/></span> */}
+            </select>
+
           </div>
         </nav>
-        
-       <Add/>
-      
-       
-      </div>
+
+        <Add />
+
+
+      </div></>
   )
 }
 
--- front-side/src/components/Shopkeeper/Products/Add.jsx
+++ front-side/src/components/Shopkeeper/Products/Add.jsx
@@ -1,9 +1,17 @@
-import React,{useState} from 'react';
+import React,{useState, useRef} from 'react';
 import "../Products/styleP.css";
 import axios from 'axios';
 import IP from "../../api/ip"
+import {BsFillCameraFill} from 'react-icons/bs'
+import Snackbar from "./snackbar";
+
+const SnackbarType = {
+  success: "success",
+  fail: "fail",
+};
 
 const AddProduct = () => {
+  const snackbarRef = useRef(null);
 
 const [formData, setFormData] = useState({
   category:"",
@@ -15,30 +23,26 @@ const [formData, setFormData] = useState({
 
 
   const add = () =>{
-    const form=new FormData()
-    Object.keys(formData).map((key)=>{
-      form.append(key,formData[key])
-    })
-    axios.post(`${IP}/product/create`,form)
-    
+    axios.post(`${IP}/product/create`,formData)
     .then((response) => {
       console.log(response.data);
     })
   }
   return (
+    
     <div className="addproduct-container">
       <div className='header-addproduct'>
-      <h3>Add Product</h3>
+      <h2 id='add'>Add Product</h2>
       </div>
-      <main>
+
         <div className='form-product'>
          <div >
           <label >Category</label>
-          <input type="text" placeholder='Canapé...' value={formData.category} onChange={event => {setFormData({...formData, category:event.target.value})}} required/>
+          <input id='inputt' type="text" placeholder='Canapé...' value={formData.category} onChange={event => {setFormData({...formData, category:event.target.value})}} required/>
          </div>
          <div >
           <label >Title</label>
-          <input type="text" placeholder='Canapé convertible...' value={formData.title} onChange={event => {setFormData({...formData, title:event.target.value})}} required/>
+          <input id='inputt' type="text" placeholder='Canapé convertible...' value={formData.title} onChange={event => {setFormData({...formData, title:event.target.value})}} required/>
          </div>
          <div >
           <label >description</label>
@@ -46,17 +50,25 @@ const [formData, setFormData] = useState({
          </div>
          <div >
           <label >Price</label>
-          <input type="number" placeholder='100' value={formData.price} onChange={event => {setFormData({...formData, price:event.target.value})}} required/>
-         </div>
-         <div >
-          <input type='file'  onChange={event => {console.log(event.target.files[0]);
-            setFormData({...formData, image:event.target.files[0]})}}/>
+          <input id='inputt' type="number" placeholder='100' value={formData.price} onChange={event => {setFormData({...formData, price:event.target.value})}} required/>
          </div>
          <div>
-         <button type='button' className='button-37' onClick={add}>ADD</button>
+          <button type='button' className='button-select'><BsFillCameraFill  id='camera'/>Choose your Image</button>
+         </div>
+         <div>
+         <button type='button' className='button-37' onClick={() => {
+          snackbarRef.current.show();
+        }}
+        >ADD
+         </button>
+         <Snackbar
+        ref={snackbarRef}
+        message="This is a success message!!"
+        type={SnackbarType.success}
+      />
          </div>
         </div>
-      </main>
+     
     </div>
   )
 }
